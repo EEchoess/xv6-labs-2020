5d2e0b15d74377f5deae59af58dfa272a13cbd26
EEchoess <1693943423@qq.com>
2026-01-30 19:13:49 -0800

    Lab4

--- kernel/proc.c
+++ kernel/proc.c
@@ -113,6 +113,15 @@ found:
     return 0;
   }
 
+  if((p->alarm_trapframe = (struct trapframe *)kalloc()) == 0){
+    release(&p->lock);
+    return 0;
+  }
+  p->alarm_interval = 0;
+  p->alarm_ticks = 0;
+  p->alarm_handler = 0;
+  p->alarm_goingoff = 0;
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -138,6 +147,8 @@ freeproc(struct proc *p)
 {
   if(p->trapframe)
     kfree((void*)p->trapframe);
+  if(p->alarm_trapframe)
+    kfree((void*)p->alarm_trapframe);
   p->trapframe = 0;
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
@@ -149,6 +160,10 @@ freeproc(struct proc *p)
   p->chan = 0;
   p->killed = 0;
   p->xstate = 0;
+  p->alarm_interval = 0;
+  p->alarm_ticks = 0;
+  p->alarm_handler = 0;
+  p->alarm_goingoff = 0;
   p->state = UNUSED;
 }
 
--- kernel/proc.h
+++ kernel/proc.h
@@ -103,4 +103,11 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+
+  int alarm_interval;
+  struct trapframe *alarm_trapframe;
+  int alarm_ticks;
+  void (*alarm_handler)();
+  int alarm_goingoff;
+
 };
