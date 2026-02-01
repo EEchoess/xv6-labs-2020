37db144be5425d9c21a309abf8dd87c794a42fa5
EEchoess <1693943423@qq.com>
2026-02-01 08:04:23 -0800

    Lab5

--- kernel/trap.c
+++ kernel/trap.c
@@ -67,10 +67,15 @@ usertrap(void)
     syscall();
   } else if((which_dev = devintr()) != 0){
     // ok
-  } else {
-    printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
-    printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
-    p->killed = 1;
+  } else{
+    uint64 fault_va = r_stval(); 
+    if((r_scause() == 13 || r_scause() == 15) && uvmshouldallocate(fault_va)){
+      uvmlazyallocate(fault_va);
+    } else {
+      printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
+      printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
+      p->killed = 1;
+    }
   }
 
   if(p->killed)
--- kernel/vm.c
+++ kernel/vm.c
@@ -5,6 +5,9 @@
 #include "riscv.h"
 #include "defs.h"
 #include "fs.h"
+#include "spinlock.h"
+#include "proc.h"
+
 
 /*
  * the kernel's page table.
@@ -181,9 +184,11 @@ uvmunmap(pagetable_t pagetable, uint64 va, uint64 npages, int do_free)
 
   for(a = va; a < va + npages*PGSIZE; a += PGSIZE){
     if((pte = walk(pagetable, a, 0)) == 0)
-      panic("uvmunmap: walk");
+      // panic("uvmunmap: walk");
+      continue;
     if((*pte & PTE_V) == 0)
-      panic("uvmunmap: not mapped");
+      // panic("uvmunmap: not mapped");
+      continue;
     if(PTE_FLAGS(*pte) == PTE_V)
       panic("uvmunmap: not a leaf");
     if(do_free){
@@ -269,6 +274,43 @@ uvmdealloc(pagetable_t pagetable, uint64 oldsz, uint64 newsz)
   return newsz;
 }
 
+int 
+uvmshouldallocate(uint64 va){
+  pte_t* pte;
+  struct proc* p = myproc();
+  return va < p->sz
+  && PGROUNDDOWN(va) != r_sp()
+  && (((pte = walk(p->pagetable, va, 0)) == 0) || ((*pte & PTE_V) == 0));
+}
+// The faulting virtual address (fault_va) is considered valid if:
+// 1) It lies just above the current user stack pointer (sp), i.e. within
+//    the next stack page, indicating a legitimate stack expansion;
+// 2) It is still below the process's maximum user address (p->sz);
+// 3) A physical page can be successfully allocated (kalloc() != 0).
+// If all conditions hold, the kernel will allocate and map a new page
+// to extend the process's user stack on demand.
+void
+uvmlazyallocate(uint64 va){
+  struct proc* p = myproc();
+  char* pa = kalloc();  
+  if(pa == 0){
+    printf("lazy alloc: out of memory");
+    p->killed = 1;
+  }
+  else{
+    memset(pa, 0, PGSIZE);
+    if(mappages(p->pagetable, PGROUNDDOWN(va), PGSIZE, (uint64)pa, PTE_R | PTE_W | PTE_X | PTE_U) != 0){
+      printf("lazy alloc: failed to map page\n");
+      kfree(pa);
+      p->killed = 1;
+    }
+  }
+}
+
+
+
+
+
 // Recursively free page-table pages.
 // All leaf mappings must already have been removed.
 void
@@ -315,9 +357,11 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
 
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walk(old, i, 0)) == 0)
-      panic("uvmcopy: pte should exist");
+      // panic("uvmcopy: pte should exist");
+      continue;
     if((*pte & PTE_V) == 0)
-      panic("uvmcopy: page not present");
+      // panic("uvmcopy: page not present");
+      continue;
     pa = PTE2PA(*pte);
     flags = PTE_FLAGS(*pte);
     if((mem = kalloc()) == 0)
@@ -355,7 +399,9 @@ int
 copyout(pagetable_t pagetable, uint64 dstva, char *src, uint64 len)
 {
   uint64 n, va0, pa0;
-
+  if(uvmshouldallocate(dstva)){
+    uvmlazyallocate(dstva);
+  }
   while(len > 0){
     va0 = PGROUNDDOWN(dstva);
     pa0 = walkaddr(pagetable, va0);
@@ -380,7 +426,9 @@ int
 copyin(pagetable_t pagetable, char *dst, uint64 srcva, uint64 len)
 {
   uint64 n, va0, pa0;
-
+  if(uvmshouldallocate(srcva)){
+    uvmlazyallocate(srcva);
+  }
   while(len > 0){
     va0 = PGROUNDDOWN(srcva);
     pa0 = walkaddr(pagetable, va0);
